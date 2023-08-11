eb40d78d0d42bb4c913d243b2e27215bd92e221f
juan <1219466209@qq.com>
2023-08-11 15:37:24 +0800

    project的上传8.11

---
+++ project/kbuild/4.9.84/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 160 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#160 SMP PREEMPT Thu Aug 10 02:36:14 UTC 2023"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "industio"
+#define LINUX_COMPILER "gcc version 8.2.1 20180802 (GNU Toolchain for the A-profile Architecture 8.2-2018-08 (arm-rel-8.23)) "
